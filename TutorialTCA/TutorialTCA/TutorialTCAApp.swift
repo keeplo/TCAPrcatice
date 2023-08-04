d0d56cc76a8117f00dea1d45663c307686f899fb
Keeplo <keepingitflow@gmail.com>
2023-08-04 10:45:40 +0900

    chore: 프로젝트 생성

---
+++ TutorialTCA/TutorialTCA/TutorialTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialTCAApp.swift
+//  TutorialTCA
+//
+//  Created by 김용우 on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct TutorialTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

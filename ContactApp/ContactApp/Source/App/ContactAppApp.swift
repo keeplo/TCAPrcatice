987054692e907586e57068bd6c69cfeb03289b06
Keeplo <keepingitflow@gmail.com>
2024-08-07 09:22:23 +0900

    chore: ContactApp 프로젝트 생성

---
+++ ContactApp/ContactApp/Source/App/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by 김용우 on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactApp/ContactApp/Source/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactApp
+//
+//  Created by 김용우 on 8/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
